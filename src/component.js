3391e36bd280cd23feb7375f4c3c751685bc49c7
Syed Muhammad Haris Ahmed <54531180+SyedMuhammadHaris@users.noreply.github.com>
2020-10-08 01:22:54 +0500

    Update component.js

--- src/component.js
+++ src/component.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FirstComponent (props){
     return (
       <div>
-         <h1>Hello World from First Student : {props.student} </h1>
+         <h1>Hello World from Student : {props.student} </h1>
          
       </div>
     );
