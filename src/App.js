c4cddbbf5593f43df13a36a58ce513efe549e058
Syed Muhammad Haris Ahmed <54531180+SyedMuhammadHaris@users.noreply.github.com>
2020-10-08 00:58:45 +0500

    Add component.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import firstComponent from './component.js'; 
 
 function App() {
   return (
     <div className="App">
      <h1>Hello World Learning React</h1>
      <h1>Hello World Learning  JavaScript Libary</h1>
+     <firstComponent />
     </div>
   );
 }
---
+++ src/component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function firstComponent(){
+    return (
+      <div>
+         <h1>Hello World from Child Component </h1>
+      </div>
+    );
+}
+
+export default firstComponent;
